25930b0d6c66d680330cf8720667785b0087486f
priyanka gupta <pynk1992@gmail.com>
2023-10-11 10:50:55 +0200

    editpage updated

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -23,6 +23,14 @@ export default async function handler(request, response) {
     response.status(200).json(place);
   }
 
+  if (request.method === "PATCH") {
+    await Place.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+  }
+
+  response.status(200).json({ status: `Place ${id} updated.` });
+
   if (request.method === "DELETE") {
     // await dbConnect()
     await Place.findByIdAndDelete(id);
--- pages/places/[id]/edit.js
+++ pages/places/[id]/edit.js
@@ -11,7 +11,14 @@ export default function EditPage() {
   const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
 
   async function editPlace(place) {
-    console.log("Place edited (but not really...)");
+    await fetch(`/api/places/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(place),
+    });
+    router.push("/");
   }
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
@@ -22,7 +29,7 @@ export default function EditPage() {
       <Link href={`/places/${id}`} passHref legacyBehavior>
         <StyledLink justifySelf="start">back</StyledLink>
       </Link>
-      <Form onSubmit={editPlace} formName={'edit-place'} defaultData={place} />
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
     </>
   );
 }
